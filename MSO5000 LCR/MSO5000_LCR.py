a71b068bbbe84b0a4bd2f39c080e18143ea25735
Lord_prei <93540498+Leondadapro@users.noreply.github.com>
2025-09-18 22:02:16 +0200

    Add settings management and improve CLI for LCR tool
    - Introduces settings management via Excel files, allowing users to load, modify, and save custom and default settings for decimal rounding and time delay.
    
    - Refactors the CLI to include a settings menu, improves text dialog structure, and updates the impedance calculation to use configurable rounding.
    
    - Adds new Excel files for settings and their backups.
    
    IMPORTANT
    There is a bug with the save changes one that does smt wierd with the Settings_Custom.xlms

--- MSO5000 LCR/MSO5000_LCR.py
+++ MSO5000 LCR/MSO5000_LCR.py
@@ -1,16 +1,27 @@
+from calendar import c
+import sys
+from turtle import clear
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import time
+import subprocess
 import numpy as np
 import math
 import pyvisa
 from enum import Enum
 
+Rounded     = 2     #Decimal places for rounding
+Time_Delay  = 1     #Time Delay for better UX
+Repeat      = 0     #Variable for repeating loops
+
 class State(Enum):
     # All of the text dialog variables
-    Start_text = 1
-    Pick_text1 = 2
-    Pick_text2 = 3
+    Start_Text = 1
+    Pick_Text1 = 2
+    Pick_Text2 = 3
+    Pick_Text3 = 4
+    Pick_Text_Setting = 5
 
     # Random ah constants
     Voltage =       0  #Voltage for Graphing
@@ -23,11 +34,31 @@ class State(Enum):
     Blind =         7  #Blindwiderstand for Graphing
 
     # Random ah variables
-    Rounded = 2  #Decimal places for rounding
 
-Repeat = 0;
+def Call_Settings(Type):    #Calling Settings from Excel File
+    if   ((Type == "Custom") or (Type == "RND")):
+        dfSettings = pd.read_excel('Settings_Custom.xlsx', header = None) #Custom Settings from Excel File
 
-def Impedance_Calculation():
+    elif (Type == "Default"):
+        dfSettings = pd.read_excel('Settings_Default.xlsx', header = None) #Default Settings from Excel File
+
+
+    Rounded = int(dfSettings.iloc[0,1])      #Decimal places for rounding
+    Time_Delay = dfSettings.iloc[1,1]   #Time Delay for better UX in seconds
+
+    if (Type == "RND"):
+        print("\nRounded Decimal Places\t\t: ", Rounded)
+        print("\nTime Delay when going back\t: ", Time_Delay)
+
+    return Rounded, Time_Delay
+
+def Save_Settings(Rounded, Time_Delay):
+    dfSettings = pd.read_excel('Settings_Custom.xlsx', header = None)
+    dfSettings.iloc[0,1] = int(Rounded)      #Decimal places for rounding
+    dfSettings.iloc[1,1] = float(Time_Delay)   #Time Delay for better UX in seconds
+    dfSettings.to_excel("Settings_Custom.xlsx", index = False) #Saving Settings to Excel File
+
+def Impedance_Calculation():    #Calculating Impedance, Resistance and Blindwiderstand from Voltage, Current, Frequency and Phase Offset
     dfCal = pd.read_excel('Clean.xlsx') #Cleaned data from MSO5000
     dfCalRounded = dfCal.copy()         #Copy of dataframe for rounded values
     Xmax = dfCal.shape[1]       #Number of columns
@@ -52,14 +83,14 @@ def Impedance_Calculation():
         dfCal.iloc[Y,X+6] = Resistance      #Storing Resistance in Ohm
         dfCal.iloc[Y,X+7] = Blind           #Storing Blindwiderstand in Ohm
 
-        Rounded_Voltage =       round(Voltage, State.Rounded)                                 #Rounding Voltage to Rounded decimal places
-        Rounded_Current =       round(Current * 1000, State.Rounded)                          #Rounding Current to Rounded decimal places and convert A to mA
-        Rounded_Frequeny =      round(Frequenzy, State.Rounded)                               #Rounding Frequency to Rounded decimal places
-        Rounded_PhaseOffset =   round(PhaseOffset, State.Rounded)                             #Rounding Phase Offset to Rounded decimal places
-        Rounded_Impedance_abs = round(Impedance_abs, State.Rounded)                           #Rounding Impedance to Rounded decimal places
-        Rounded_Impedance =     round(Resistance, State.Rounded) + round(Blind, State.Rounded)*1j   #Rounding Complex Impedance to Rounded decimal places
-        Rounded_Resistance =    round(Resistance, State.Rounded)                              #Rounding Resistance to Rounded decimal places
-        Rounded_Blind =         round(Blind, State.Rounded)                                   #Rounding Blindwiderstand to Rounded decimal places
+        Rounded_Voltage =       round(Voltage, Rounded)                                 #Rounding Voltage to Rounded decimal places
+        Rounded_Current =       round(Current * 1000, Rounded)                          #Rounding Current to Rounded decimal places and convert A to mA
+        Rounded_Frequeny =      round(Frequenzy, Rounded)                               #Rounding Frequency to Rounded decimal places
+        Rounded_PhaseOffset =   round(PhaseOffset, Rounded)                             #Rounding Phase Offset to Rounded decimal places
+        Rounded_Impedance_abs = round(Impedance_abs, Rounded)                           #Rounding Impedance to Rounded decimal places
+        Rounded_Impedance =     round(Resistance, Rounded) + round(Blind, Rounded)*1j   #Rounding Complex Impedance to Rounded decimal places
+        Rounded_Resistance =    round(Resistance, Rounded)                              #Rounding Resistance to Rounded decimal places
+        Rounded_Blind =         round(Blind, Rounded)                                   #Rounding Blindwiderstand to Rounded decimal places
 
         dfCalRounded.iloc[Y,X]   = Rounded_Voltage
         dfCalRounded.iloc[Y,X+1] = Rounded_Current
@@ -76,50 +107,164 @@ def Impedance_Calculation():
 
 def TXT_Dialog(n):                   #All of the text dialog stuff
     match n:
-        case State.Start_text:
+        case State.Start_Text:
             print(  "Hello and Welcome to the MSO5000 LCR Measurement Tool\n"
                     "This tool helps you to measure and analyze LCR components with the MSO5000\n\n\n")
 
-        case State.Pick_text1:
+        case State.Pick_Text1:
             print(  "What do u wanna do? (Pick from List)\n\n")
             print(  "1 : Measure LCR Component\n"
                     "2 : Analyze / Calculate existing Measurement\n"
+                    "3 : Settings\n"
                     "99: Exit Program\n\n")
 
-        case State.Pick_text2:
+        case State.Pick_Text2:
             print(  "What do u wanna do? (Pick from List)\n\n")
             print(  "1 : Calculate Data and export as Excel Files\n"
                     "2 : Plot Data\n"
                     "3 : Both Calculate and Plot Data\n"
-                    "99: Exit to Main Menu\n\n")
+                    "99: Go back\n\n")
 
-def clear():
-    os.system('cls')
-    
+        case State.Pick_Text3:
+            print(  "Settings Menu\n\n"
+                    "1 : Load Default Settings\n"
+                    "2 : Load Custom Settings\n"
+                    "3 : Show Current Settings\n"
+                    "4 : Change Settings\n"
+                    "5 : Save Current Settings as Custom Settings\n"
+                    "99: Go back\n\n")
 
+        case State.Pick_Text_Setting:
+            print(  "What Settings do you wanna change?\n\n"
+                    "1 : Decimal places for rounding (Current: " + str(Rounded) + ")\n"
+                    "2 : Time Delay when going back (Current: " + str(Time_Delay) + "s)\n"
+                    "99: Go back\n\n")
+
+def Clear_CLI():
+    print("\033[2J\033[H", end='')  # Clear screen + move cursor to top-left
+
+Rounded, Time_Delay = Call_Settings("Custom")  #Calling Custom Settings at the start of the program
+Clear_CLI()
 
 while True:
-    TXT_Dialog(State.Start_text)    # Starting Text
-    TXT_Dialog(State.Pick_text1)    # pick from list text
+    Clear_CLI()
+    TXT_Dialog(State.Start_Text)    # Starting Text
+    TXT_Dialog(State.Pick_Text1)    # pick from list text
 
     n = input("Your Input: ")       # User Input
     
     match n:
         case "1":
+
+            Clear_CLI()
             print("Measure LCR Component")
             
-            
-
         case "2":
+
+            Clear_CLI()
             print("Analyze / Calculate existing Measurement")
 
             Repeat = 1
             while (Repeat == 1):
-                TXT_Dialog(State.Pick_text2)            # pick from list text
+                TXT_Dialog(State.Pick_Text2)            # pick from list text
                 User_Input = input("Your Input: ")      # User Input
-                if(User_Input == "99"):
-                    Repeat = 0
-            
+                
+                match User_Input:
+
+                    case "1":
+                        Clear_CLI()
+                        print("Calculate Data and export as Excel Files")
+
+                    case "2":
+                        Clear_CLI()
+                        print("Plot Data")
+
+                    case "3":
+                        Clear_CLI()
+                        print("Both Calculate and Plot Data")
+
+                    case "99":
+                        Clear_CLI()
+                        Repeat = 0
+                        print("Exit to Main Menu")
+                        time.sleep(Time_Delay)
+
+        case "3":
+            Clear_CLI()
+            Repeat = 1
+            while (Repeat == 1):
+                Clear_CLI()
+                TXT_Dialog(State.Pick_Text3)
+                User_Input = input("Your Input: ")
+
+                match User_Input:
+                    case "1":   #Load Default Settings
+                        Clear_CLI()
+                        print("Loaded Default Settings")
+                        Rounded, Time_Delay = Call_Settings("Default")
+                        time.sleep(Time_Delay)
+
+                    case "2":   #Load Custom Settings
+                        Clear_CLI()
+                        print("Loaded Custom Settings")
+                        Rounded, Time_Delay = Call_Settings("Custom")
+                        time.sleep(Time_Delay)
+
+                    case "3":   #Show Current Settings
+                        Clear_CLI()
+                        Rounded, Time_Delay = Call_Settings("RND") #Calling current settings
+                        time.sleep(5)
+
+                    case "4":   #Change Settings
+                        again = 1
+                        while (again == 1):
+                            again = 0
+                            Clear_CLI()
+                            print("Change Settings")
+                            TXT_Dialog(State.Pick_Text_Setting)
+                            User_Input = input("Your Input: ")
+
+                            match User_Input:
+                                case "1":   #Change Decimal Places
+                                    Clear_CLI()
+                                    print("Change Decimal Places")
+                                    New_Value = input("Enter new value (Current: " + str(Rounded) + "): ")
+                                    Rounded = int(New_Value)
+                                    print("Decimal Places changed to: ", Rounded)
+                                    print("\n\ndo you wana change more settings?")
+                                    again = int(input("2 = yes / 1 = no: ")) - 1
+
+                                case "2":   #Change Time Delay
+                                    Clear_CLI()
+                                    print("Change Time Delay")
+                                    New_Value = input("Enter new value (Current: " + str(Time_Delay) + "s): ")
+                                    Time_Delay = float(New_Value)
+                                    print("Time Delay changed to: ", Time_Delay, "s")
+                                    print("\n\ndo you wana change more settings?")
+                                    again = int(input("2 = yes / 1 = no: ")) - 1
+
+                                case "99":
+                                    Clear_CLI()
+                                    again = 0
+                                    print("Go back")
+                                    time.sleep(Time_Delay)
+
+                    case "5":   #Save Current Settings as Custom Settings
+                        Clear_CLI()
+                        print("Save Current Settings as Custom Settings")
+                        Save_Settings(Rounded, Time_Delay)
+                        print("\nCurrent Settings saved as Custom Settings")
+                        time.sleep(Time_Delay)
+
+                    case "99":
+                        Clear_CLI()
+                        Repeat = 0
+                        print("Exit to Main Menu")
+                        time.sleep(Time_Delay)
+
         case "99":
+            Clear_CLI()
             print("Exit Program")
+            time.sleep(Time_Delay) #Short Delay for better UX
+            sys.exit()      #Exit Program
 
